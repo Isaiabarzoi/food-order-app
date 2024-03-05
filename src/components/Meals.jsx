ab9367f690af836443e0c72d95646eab9632426a
isaiabarzoi <isaiabarzoi@gmail.com>
2024-03-05 16:17:45 +0100

    added filter to order meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MealItem from "./MealItem";
 import Loader from "./UI/Loader";
 import Button from "./UI/Button";
@@ -15,9 +15,29 @@ export default function Meals() {
     error,
   } = useHttp("http://localhost:3000/meals", config, []);
   const [showHistory, setShowHistory] = useState(false);
+  const [sortedMeals, setSortedMeals] = useState(availableMeals);
+  const [sortBy, setSortBy] = useState("Default");
+
+  useEffect(() => {
+    if (availableMeals) {
+      setSortedMeals([...availableMeals]);
+    }
+  }, [availableMeals]);
 
   function handleOpenHistoryOrder() {
     setShowHistory(true);
+  }
+
+  function handleSortChange(e) {
+    const selectedOption = e.target.value;
+    setSortBy(selectedOption);
+    let sortedMeals = [...availableMeals];
+    if (selectedOption === "From most expensive") {
+      sortedMeals.sort((a, b) => b.price - a.price);
+    } else if (selectedOption === "From the cheapest") {
+      sortedMeals.sort((a, b) => a.price - b.price);
+    }
+    setSortedMeals(sortedMeals);
   }
 
   if (error) {
@@ -36,9 +56,18 @@ export default function Meals() {
     <>
       <div className="history-action">
         <Button onClick={handleOpenHistoryOrder}>Order History</Button>
+        <select
+          value={sortBy}
+          onChange={handleSortChange}
+          className="select-actions"
+        >
+          <option value="Default">Default</option>
+          <option value="From most expensive">From most expensive</option>
+          <option value="From the cheapest">From the cheapest</option>
+        </select>
       </div>
       <ul id="meals">
-        {availableMeals.map((meal) => (
+        {sortedMeals.map((meal) => (
           <MealItem key={meal.id} meal={meal} />
         ))}
       </ul>
